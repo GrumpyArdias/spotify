9e3c3d84e5a1f2e866eb575f70b4f12b03650176
GrumpyArdias <this.mario@outlook.com>
2022-01-25 05:17:59 +0100

    Stablishing token management

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,28 @@
 import nextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
+
+async function refreshAccessToken(token) {
+  try {
+    spotifyApi.setAccesToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+    const { body: refreshToken } = await spotifyApi.refreshAccessToken();
+    console.log("REFRESEH TOKEN IS", refreshToken);
+
+    return {
+      ...token,
+      accessToken: refreshAccessToken.access_token,
+      accessTokenExpires: Date.now + refreshAccessToken.expires_in * 1000,
+      refreshToken: refreshToken.refresh_token ?? token.refreshToken,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export default nextAuth({
   providers: [
@@ -9,4 +32,27 @@ export default nextAuth({
       authorization: LOGIN_URL,
     }),
   ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
+        };
+      }
+      if (Date.now() < token.accessTokenExpires) {
+        console.log("TOKEN DE ACCESO VALIDO");
+        return token;
+      }
+      console.log("TOKEN DE ACCESO CADUCADO");
+      return await refreshAccessToken(token);
+    },
+  },
 });
